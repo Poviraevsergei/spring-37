e5292787042f1213004599a1c5d38accc7db719e
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2026-01-12 21:40:00 +0300

    Xml bean init

--- src/main/java/com/poviraev/Main.java
+++ src/main/java/com/poviraev/Main.java
@@ -3,16 +3,20 @@ package com.poviraev;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         //Создание Spring контейнера(ApplicationContext)
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-settings.xml");
 
         //Достаем из контекста Бин Car с названием Adam
-        Map<String, Car> car = context.getBeansOfType(Car.class);
+        Car car = (Car) context.getBean("Adam");
+        Car car1 = (Car) context.getBean("Adam");
 
-        System.out.println(car);
+        System.out.println(car.hashCode());
+        System.out.println(car1.hashCode());
+
+        System.out.println(car.getId());
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
     }
 }
